07f255e5dac56d16e80be7eedb4bc26c8080c8e0
4bestoption <145018454+4bestoption@users.noreply.github.com>
2023-09-20 20:16:51 +0900

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,10 @@ public class Player : MonoBehaviour
  
     public float moveSpeed = 1;
 
+    Animator animator = null;
+    public GameObject playerObject = null;
+    public float HP = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -58,7 +62,31 @@ public class Player : MonoBehaviour
             GetComponent<Animator>().SetTrigger("isAttack");
             
         }
-        
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            // HP가 깍인다. hp = hp - 상대방의 공격력
+            HP = HP - 10;
+            // 애니메이션 출력 (피격)
+            if (HP <= 0)
+            {
+                Death();
+            }
+            else
+            {
+                animator.SetTrigger("isHit");
+            }
+            // 만약 HP가 0 이하라면 죽어야한다.
+        }
+
+
+        private void Death()
+        {
+            animator.SetBool("isDeath", true);
+        }
+
+
+
 
     }
 }
